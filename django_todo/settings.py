6bea8e33c3b8eb69093eee2d6cb0e07eefbb24c2
rachel-o-donnell <hellotherachelo@gmail.com>
2022-11-19 17:32:16 +0000

    Start testing

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
-import os
-if os.path.isfile('env.py'):
-    import env
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
